0648ecd81f0b1ffbfdea8e3748377f7593e12bb4
ekram <ekramdba@gmail.com>
2025-01-26 15:43:30 +0530

    seperated routes

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+const path = 'https://api.themoviedb.org/3/'
 
-const path = 'https://api.themoviedb.org/3/';
-
-router.get('/fetchCredits', async (req, res) => {
+router.get('/fetchTrending', async (req, res) => {
     try {
-        const { media_type, id } = req.query;
-        const response = await axios.get(`${path}${media_type}/${id}/credits?api_key=${process.env.REACT_router_API_KEY}&language=en-US`);
-        res.send(response.data);
+        const { data } = await axios.get(`${path}trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${req.query.page}`);
+        res.json(data);
     } catch (error) {
         res.status(500);
     }
@@ -16,35 +14,37 @@ router.get('/fetchCredits', async (req, res) => {
 router.get('/fetchMovies', async (req, res) => {
     try {
         const { genreforURL, page } = req.query;
-        const { data } = await axios.get(`${path}discover/movie?api_key=${process.env.REACT_router_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
+        const { data } = await axios.get(`${path}discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
         res.json(data);
     } catch (error) {
         res.status(500);
     }
 });
 
-router.post('/fetchSeries', async (req, res) => {
+router.get('/fetchSeries', async (req, res) => {
     try {
-        const { genreforURL, page } = req.body.body;
-        const { data } = await axios.get(`${path}discover/tv?api_key=${process.env.REACT_router_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
+        const { genreforURL, page } = req.query;
+        const { data } = await axios.get(`${path}discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
         res.json(data);
     } catch (error) {
         res.status(500);
     }
 });
 
-router.get('/fetchTrending', async (req, res) => {
+router.get('/fetchGenres', async (req, res) => {
     try {
-        const { data } = await axios.get(`${path}trending/all/day?api_key=${process.env.REACT_router_API_KEY}&page=${req.query.page}`);
-        res.json(data);
+        const { type } = req.query;
+        const { data } = await axios.get(`${path}genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+        res.send(data);
     } catch (error) {
         res.status(500);
     }
 });
+
 router.get('/fetchData', async (req, res) => {
     try {
         const { media_type, id } = req.query;
-        const { data } = await axios.get(`${path}${media_type}/${id}?api_key=${process.env.REACT_router_API_KEY}&language=en-US`);
+        const { data } = await axios.get(`${path}${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
         res.send(data);
     } catch (error) {
         res.status(500);
@@ -54,18 +54,18 @@ router.get('/fetchData', async (req, res) => {
 router.get('/fetchVideo', async (req, res) => {
     try {
         const { media_type, id } = req.query;
-        const { data } = await axios.get(`${path}${media_type}/${id}/videos?api_key=${process.env.REACT_router_API_KEY}&language=en-US`);
+        const { data } = await axios.get(`${path}${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
         res.send(data);
     } catch (error) {
         res.status(500);
     }
 });
 
-router.get('/fetchGenres', async (req, res) => {
+router.get('/fetchCredits', async (req, res) => {
     try {
-        const { type } = req.query;
-        const { data } = await axios.get(`${path}genre/${type}/list?api_key=${process.env.REACT_router_API_KEY}&language=en-US`);
-        res.send(data);
+        const { media_type, id } = req.query;
+        const response = await axios.get(`${path}${media_type}/${id}/credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+        res.send(response.data);
     } catch (error) {
         res.status(500);
     }
@@ -74,11 +74,10 @@ router.get('/fetchGenres', async (req, res) => {
 router.get('/fetchSearch', async (req, res) => {
     try {
         const { type, searchText, page } = req.query;
-        const { data } = await axios.get(`${path}search/${type}?api_key=${process.env.REACT_router_API_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`);
+        const { data } = await axios.get(`${path}search/${type}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`);
         res.json(data);
     } catch (error) {
         res.status(500);
     }
 });
-
 module.exports = router;
